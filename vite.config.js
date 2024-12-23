c41596e40e985d5964d652227584180bcee2c5bf
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2024-12-23 16:59:46 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-app/',
   build: {
     sourcemap: true, // Включает генерацию sourcemaps при сборке
   },
