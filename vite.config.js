5463a8dacf4e5c865a78f3db17dd293b61899a9f
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2025-01-09 23:49:39 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/todo-app/',
-  build: {
-    sourcemap: true,
-  },
   css: {
     devSourcemap: true,
   },
@@ -25,4 +22,4 @@ export default defineConfig({
       },
     ],
   },
-})
+});
